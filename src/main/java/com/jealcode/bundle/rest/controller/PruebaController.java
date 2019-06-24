e60ccd469ae79f69d2654c4dd1450dde28be38d4
Jesus Home <jeal1511@home.com>
2019-06-23 23:10:16 -0300

    agregando metodo post con parametros

--- src/main/java/com/jealcode/bundle/rest/controller/PruebaController.java
+++ src/main/java/com/jealcode/bundle/rest/controller/PruebaController.java
@@ -5,6 +5,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import com.jealcode.bundle.rest.model.Prueba;
@@ -15,24 +16,24 @@ import com.jealcode.bundle.rest.model.Prueba;
 @RequestMapping("/api")
 public class PruebaController {
 
-	@PostMapping("/prueba")
-	public ResponseEntity<Prueba> setPrueba() {
-		try {
-			Prueba prueba = new Prueba("post", 19);
-			return new ResponseEntity<>(prueba, HttpStatus.OK);
-		} catch (Exception e) {
-			return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
-		}
-	}
-	
+
 	@GetMapping("/prueba")
 	public ResponseEntity<Prueba> getPrueba() {
 		try {
-			Prueba prueba = new Prueba("Jesus", 19);
+			Prueba prueba = new Prueba("@GetMapping", 1);
 			return new ResponseEntity<>(prueba, HttpStatus.OK);
 		} catch (Exception e) {
-			return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
+			return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
 		}
 	}
-
+	
+	@PostMapping(value = "/prueba")
+	public ResponseEntity<Prueba> postCustomer(@RequestBody Prueba prueba) {
+		try {
+			System.out.println(prueba);
+			return new ResponseEntity<>(prueba, HttpStatus.CREATED);
+		} catch (Exception e) {
+			return new ResponseEntity<>(HttpStatus.EXPECTATION_FAILED);
+		}
+	}
 }
--- src/test/java/com/jealcode/bundle/rest/PruebaController.java
+++ src/test/java/com/jealcode/bundle/rest/PruebaController.java
@@ -11,18 +11,29 @@ import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.client.methods.HttpUriRequest;
+import org.apache.http.entity.ContentType;
+import org.apache.http.entity.StringEntity;
 import org.apache.http.impl.client.HttpClientBuilder;
+import org.apache.http.params.BasicHttpParams;
+import org.apache.http.params.HttpParams;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import com.jealcode.bundle.rest.model.Prueba;
+
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class PruebaController {
 	@Test
-	public void setPrueba_simpleTest() throws ClientProtocolException, IOException{
-	    HttpUriRequest httpUriRequest = new HttpPost( "http://localhost:8080/api/prueba");
+	public void postPrueba_simpleTest() throws ClientProtocolException, IOException{
+		Prueba prueba = new Prueba("prueba", 2);
+	
+   StringEntity stringEntity = new StringEntity(prueba.toString(),
+	                ContentType.APPLICATION_JSON);
+	    HttpUriRequest httpUriRequest = new HttpPost("http://localhost:8080/api/prueba");
+	   // httpUriRequest.setParams(new HTTPpsetParameter("value", prueba.toString());
 	    HttpResponse httpResponse = HttpClientBuilder.create().build().execute(httpUriRequest);
 	    assertThat(httpResponse.getStatusLine().getStatusCode(),equalTo(HttpStatus.SC_OK));
 	}
